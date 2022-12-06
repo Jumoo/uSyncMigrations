f8e1b7a6858909f267e2cd4af8f8a9ac24967d07
Kevin Jump <kevin@thejumps.co.uk>
2022-12-06 07:31:40 +0000

    fix case of Textbox to TextBox when migrating from v7

--- uSync.Migrations/Migrators/Core/TextBoxMigrator.cs
+++ uSync.Migrations/Migrators/Core/TextBoxMigrator.cs
@@ -1,7 +1,18 @@
 ﻿using Umbraco.Cms.Core.PropertyEditors;
 
+using uSync.Migrations.Migrators.Models;
+using uSync.Migrations.Models;
+
 namespace uSync.Migrations.Migrators;
 
 [SyncMigrator(UmbConstants.PropertyEditors.Aliases.TextBox, typeof(TextboxConfiguration))]
 public class TextBoxMigrator : SyncPropertyMigratorBase
-{ }
+{
+
+    /// <summary>
+    ///  in v8 the textbox changes from Textbox to TextBox, 
+    /// </summary>
+    public override string GetEditorAlias(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
+        => UmbConstants.PropertyEditors.Aliases.TextBox;
+
+}
