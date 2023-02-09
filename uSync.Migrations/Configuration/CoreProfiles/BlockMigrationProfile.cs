a6f89f8519adc77d3080e2311ddbdfefb5736a34
Kevin Jump <kevin@thejumps.co.uk>
2023-02-09 15:12:35 +0000

    Tidy up the profile options

--- uSync.Migrations/Configuration/CoreProfiles/BlockMigrationProfile.cs
+++ uSync.Migrations/Configuration/CoreProfiles/BlockMigrationProfile.cs
@@ -4,6 +4,7 @@ using Umbraco.Cms.Core.Composing;
 
 using uSync.Migrations.Composing;
 using uSync.Migrations.Configuration.Models;
+using uSync.Migrations.Migrators.BlockGrid;
 using uSync.Migrations.Migrators.Optional;
 
 namespace uSync.Migrations.Configuration.CoreProfiles;
@@ -19,22 +20,23 @@ internal class BlockMigrationProfile : ISyncMigrationProfile
 
     public int Order => 200;
 
-	public string Name => "Nested Content To BlockLists";
+	public string Name => "Convert to BlockLists and BlockGrid";
 
     public string Icon => "icon-brick color-green";
 
-    public string Description => "Convert Nested content to BlockList (Experimental!)";
+    public string Description => "Convert Nested content and Grid to BlockList and BlockGrid  (Experimental!)";
 
     public MigrationOptions Options => new MigrationOptions
     {
         Group = "Convert",
-        Source = "uSync/grid-v9",
-        Target = $"{uSyncMigrations.MigrationFolder}/blocks",
-        Handlers = _migrationHandlers.SelectGroup(8, string.Empty),
+        Source = "uSync/v9",
+		Target = $"{uSyncMigrations.MigrationFolder}/{DateTime.Now:yyyyMMdd_HHmmss}",
+		Handlers = _migrationHandlers.SelectGroup(8, string.Empty),
         SourceVersion = 8,
         PreferredMigrators = new Dictionary<string, string>
         {
-            { Umbraco.Cms.Core.Constants.PropertyEditors.Aliases.NestedContent, nameof(NestedToBlockListMigrator) }
+			{ UmbConstants.PropertyEditors.Aliases.NestedContent, nameof(NestedToBlockListMigrator) },
+			{ UmbConstants.PropertyEditors.Aliases.Grid, nameof(GridToBlockGridMigrator) }
         }
     };
 }
