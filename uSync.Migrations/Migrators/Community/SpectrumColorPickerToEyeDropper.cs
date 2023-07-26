97a5c8f15a6ad69201aa9b057c123748c9997051
Arkadiusz biel <arkadiusz.biel@etchuk.com>
2023-07-26 14:37:38 +0100

    fix: ensured to convert colors to supported format

--- uSync.Migrations/Migrators/Community/SpectrumColorPickerToEyeDropper.cs
+++ uSync.Migrations/Migrators/Community/SpectrumColorPickerToEyeDropper.cs
@@ -1,18 +1,55 @@
+using System.Drawing;
+using System.Text.RegularExpressions;
 using Umbraco.Cms.Core.PropertyEditors;
 using uSync.Migrations.Context;
 using uSync.Migrations.Extensions;
 using uSync.Migrations.Migrators.Models;
 
 namespace uSync.Migrations.Migrators.Community;
-[SyncMigrator("Spectrum.Color.Picker")]
 
+[SyncMigrator("Spectrum.Color.Picker")]
 public class SpectrumColorPickerToEyeDropper : SyncPropertyMigratorBase
 {
     public override string? GetContentValue(SyncMigrationContentProperty contentProperty, SyncMigrationContext context)
-        => contentProperty.Value;
+    {
+        var raw = contentProperty.Value;
+        if (string.IsNullOrWhiteSpace(raw))
+        {
+            return raw;
+        }
+
+        if (raw.StartsWith("#"))
+        {
+            return raw;
+        }
+
+        if (raw.Length == 6)
+        {
+            return $"#{raw}";
+        }
+
+        if (raw.Contains("rgb"))
+        {
+            var colourMatches = Regex.Match(raw,
+                @"(rgba|rgb)\(\s?(\d{1,3})\,\s?(\d{1,3})\,\s?(\d{1,3})(\,\s?(\d|\d\.\d+))?\s?\)");
+
+            if (colourMatches.Groups.Count < 5) return raw;
+            var color = Color.FromArgb(
+                (int)Math.Round((double.TryParse(colourMatches.Groups[4]?.ToString(), out double parsed)
+                    ? parsed
+                    : (double)1) * 255), int.Parse(colourMatches.Groups[2].ToString()),
+                int.Parse(colourMatches.Groups[3].ToString()), int.Parse(colourMatches.Groups[4].ToString()));
+            return ColorTranslator.ToHtml(color);
+        }
+
+        return raw;
+    }
+
     public override string GetEditorAlias(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
         => UmbConstants.PropertyEditors.Aliases.ColorPickerEyeDropper;
-    public override object? GetConfigValues(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
+
+    public override object? GetConfigValues(SyncMigrationDataTypeProperty dataTypeProperty,
+        SyncMigrationContext context)
     {
         var config = new EyeDropperColorPickerConfiguration();
 
@@ -20,6 +57,11 @@ public class SpectrumColorPickerToEyeDropper : SyncPropertyMigratorBase
         {
             { "enableTransparency", nameof(config.ShowAlpha) }
         };
+        var palet = dataTypeProperty.PreValues?.FirstOrDefault(p => p.Alias == "palette")?.Value;
+        if (!string.IsNullOrWhiteSpace(palet))
+        {
+            config.ShowPalette = true;
+        }
 
         return config.MapPreValues(dataTypeProperty.PreValues, mappings);
     }
