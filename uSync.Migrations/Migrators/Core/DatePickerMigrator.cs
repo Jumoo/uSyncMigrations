23e3fdfcdebfdb130d33f61ca654aa25aa087479
Michael Chart <michael.chart@etchuk.com>
2023-09-04 14:53:17 +0100

    fix: ensure Umbraco.Date editors are migrated to Umbraco.DateTime
    Umbraco.Date no longer exists in Umbraco, so Umbraco.Date properties can be migrated to Umbraco.DateTime.

--- uSync.Migrations/Migrators/Core/DatePickerMigrator.cs
+++ uSync.Migrations/Migrators/Core/DatePickerMigrator.cs
@@ -1,8 +1,13 @@
 ﻿using Umbraco.Cms.Core.PropertyEditors;
+using uSync.Migrations.Context;
+using uSync.Migrations.Migrators.Models;
 
 namespace uSync.Migrations.Migrators;
 
 [SyncMigrator(UmbConstants.PropertyEditors.Aliases.DateTime, typeof(DateTimeConfiguration), IsDefaultAlias = true)]
 [SyncMigrator("Umbraco.Date")]
 public class DatePickerMigrator : SyncPropertyMigratorBase
-{ }
+{ 
+    public override string GetEditorAlias(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
+        => UmbConstants.PropertyEditors.Aliases.DateTime;
+}
