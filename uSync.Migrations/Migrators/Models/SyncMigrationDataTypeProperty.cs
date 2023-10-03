6a987d26d6d21f757e63d4ce4fbf36d1b49b103c
Arkadiusz Biel - bielu <bielu@bielu.pl>
2023-10-03 16:10:42 +0100

    fix: ensured that we can migrate grid

--- uSync.Migrations/Migrators/Models/SyncMigrationDataTypeProperty.cs
+++ uSync.Migrations/Migrators/Models/SyncMigrationDataTypeProperty.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Umbraco.Extensions;
 using uSync.Migrations.Models;
 
 namespace uSync.Migrations.Migrators.Models;
@@ -11,6 +15,21 @@ public sealed class SyncMigrationDataTypeProperty : SyncMigrationPropertyBase
         DatabaseType = databaseType;
         DataTypeAlias = dataTypeAlias;
         PreValues = new ReadOnlyCollection<PreValue>(preValues);
+        ConfigAsString = TranslatePreValuesToConfig(preValues);
+    }
+
+    private string? TranslatePreValuesToConfig(IList<PreValue> preValues)
+    {
+        var json = new Dictionary<string, object>();
+        foreach (var oPreValue in preValues)
+        {
+            json.TryAdd(oPreValue.Alias, oPreValue.Value.ToString().DetectIsJson() ? JsonConvert.DeserializeObject(oPreValue.Value) : oPreValue);
+        }
+        XElement xml = new XElement("Method", 
+            JsonConvert.SerializeObject(json)
+            
+        );
+        return  xml.Value;
     }
 
     public SyncMigrationDataTypeProperty(string dataTypeAlias, string editorAlias, string databaseType, string? config)
