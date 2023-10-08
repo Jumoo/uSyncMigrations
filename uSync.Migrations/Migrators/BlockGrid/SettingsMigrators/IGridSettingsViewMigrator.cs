29f4e542e42cc61f670bad1038be4ac607a1417f
Arkadiusz Biel - bielu <bielu@bielu.pl>
2023-10-08 19:42:58 +0100

    chore: adjusted code by ryan-woodruff to use context of migration instead of umbraco services

---
+++ uSync.Migrations/Migrators/BlockGrid/SettingsMigrators/IGridSettingsViewMigrator.cs
@@ -0,0 +1,30 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Extensions;
+
+namespace uSync.Migrations.Migrators.BlockGrid.SettingsMigrator;
+
+public interface IGridSettingsViewMigrator : IDiscoverable
+{
+    string ViewKey { get; }
+    string NewDataTypeAlias { get; }
+    public object ConvertContentString(string value);
+}
+
+public class GridSettingsViewMigratorCollectionBuilder
+    : LazyCollectionBuilderBase<GridSettingsViewMigratorCollectionBuilder, GridSettingsViewMigratorCollection, IGridSettingsViewMigrator>
+{
+    protected override GridSettingsViewMigratorCollectionBuilder This => this;
+}
+
+public class GridSettingsViewMigratorCollection
+    : BuilderCollectionBase<IGridSettingsViewMigrator>
+{
+    public GridSettingsViewMigratorCollection(Func<IEnumerable<IGridSettingsViewMigrator>> items) : base(items)
+    {}
+
+    public IGridSettingsViewMigrator? GetMigrator(string? viewKey)
+    {
+        if (viewKey == null) return null;
+        return this.FirstOrDefault(x => x.ViewKey.InvariantEquals(viewKey));
+    }
+}
