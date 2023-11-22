6939a8b884c40534342b6c9492392b1aba8f9d74
Steve Temple <steve@gibedigital.com>
2023-11-22 09:53:08 +0000

    Add migrator for Gibe.LinkPicker

---
+++ uSync.Migrations.Migrators/Community/GibeLinkPicker/Models/GibeLinkPickerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uSync.Migrations.Migrators.Community.GibeLinkPicker.Models
+{
+	public class GibeLinkPickerData
+	{
+		public int Id { get; set; }
+		public string Uid { get; set; }
+		public string Name { get; set; }
+		public string Url { get; set; }
+		public string Target { get; set; }
+	}
+}
