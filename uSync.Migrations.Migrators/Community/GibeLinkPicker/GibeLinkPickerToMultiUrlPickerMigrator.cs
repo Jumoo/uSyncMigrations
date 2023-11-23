97a3dc9617ddcef27c81a9472852b6d4fe131eb9
Steve Temple <steve@gibedigital.com>
2023-11-23 15:05:58 +0000

    Match formatting of GibeLinkPicker migrator to library

--- uSync.Migrations.Migrators/Community/GibeLinkPicker/GibeLinkPickerToMultiUrlPickerMigrator.cs
+++ uSync.Migrations.Migrators/Community/GibeLinkPicker/GibeLinkPickerToMultiUrlPickerMigrator.cs
@@ -6,11 +6,11 @@ using uSync.Migrations.Migrators.Community.GibeLinkPicker.Models;
 
 namespace uSync.Migrations.Migrators.Community.GibeLinkPicker
 {
-	[SyncMigrator("Gibe.LinkPicker")]
-	public class GibeLinkPickerToMultiUrlPickerMigrator : SyncPropertyMigratorBase
-	{
-		public override string GetEditorAlias(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
-				=> UmbConstants.PropertyEditors.Aliases.MultiUrlPicker;
+    [SyncMigrator("Gibe.LinkPicker")]
+    public class GibeLinkPickerToMultiUrlPickerMigrator : SyncPropertyMigratorBase
+    {
+        public override string GetEditorAlias(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
+                => UmbConstants.PropertyEditors.Aliases.MultiUrlPicker;
 
         public override object? GetConfigValues(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
         {
@@ -22,55 +22,55 @@ namespace uSync.Migrations.Migrators.Community.GibeLinkPicker
         }
 
         public override string GetContentValue(SyncMigrationContentProperty contentProperty, SyncMigrationContext context)
-		{
-			if (contentProperty?.Value == null)
-			{
-				return contentProperty?.Value;
-			}
+        {
+            if (contentProperty?.Value == null)
+            {
+                return contentProperty?.Value;
+            }
 
-			var gibeLinkPickers = GetPickerValues(contentProperty.Value).ToArray();
-			
-			if (!gibeLinkPickers.Any())
-			{
-				return contentProperty.Value;
-			}
+            var gibeLinkPickers = GetPickerValues(contentProperty.Value).ToArray();
 
-			var links = new List<MultiUrlPickerValueEditor.LinkDto>();
+            if (!gibeLinkPickers.Any())
+            {
+                return contentProperty.Value;
+            }
 
-			foreach (var picker in gibeLinkPickers)
-			{
-				var link = new MultiUrlPickerValueEditor.LinkDto
-				{
-					Name = picker?.Name ?? string.Empty,
-					Url = picker?.Url ?? string.Empty,
-					Udi = picker?.Uid != null ? new GuidUdi(UmbConstants.UdiEntityType.Document, Guid.Parse(picker.Uid)) : null,
-				};
+            var links = new List<MultiUrlPickerValueEditor.LinkDto>();
 
-				if (picker?.Target == "_blank")
-				{
-					link.Target = picker.Target;
-				}
+            foreach (var picker in gibeLinkPickers)
+            {
+                var link = new MultiUrlPickerValueEditor.LinkDto
+                {
+                    Name = picker?.Name ?? string.Empty,
+                    Url = picker?.Url ?? string.Empty,
+                    Udi = picker?.Uid != null ? new GuidUdi(UmbConstants.UdiEntityType.Document, Guid.Parse(picker.Uid)) : null,
+                };
 
-				links.Add(link);
-			}
-			return JsonConvert.SerializeObject(links, Formatting.Indented);
-		}
-		
-		private IEnumerable<GibeLinkPickerData> GetPickerValues(string? contentValue)
-		{
-			if (contentValue == null)
-			{
-				return Enumerable.Empty<GibeLinkPickerData>();
-			}
+                if (picker?.Target == "_blank")
+                {
+                    link.Target = picker.Target;
+                }
 
-			if (contentValue.StartsWith("["))
-			{
-				return JsonConvert.DeserializeObject<IEnumerable<GibeLinkPickerData>>(contentValue) 
-				       ?? Enumerable.Empty<GibeLinkPickerData>();
-			}
-			return JsonConvert.DeserializeObject<GibeLinkPickerData>(contentValue)?.AsEnumerableOfOne() 
-			       ?? Enumerable.Empty<GibeLinkPickerData>();
-		}
-	}
+                links.Add(link);
+            }
+            return JsonConvert.SerializeObject(links, Formatting.Indented);
+        }
+
+        private IEnumerable<GibeLinkPickerData> GetPickerValues(string? contentValue)
+        {
+            if (contentValue == null)
+            {
+                return Enumerable.Empty<GibeLinkPickerData>();
+            }
+
+            if (contentValue.StartsWith("["))
+            {
+                return JsonConvert.DeserializeObject<IEnumerable<GibeLinkPickerData>>(contentValue)
+                       ?? Enumerable.Empty<GibeLinkPickerData>();
+            }
+            return JsonConvert.DeserializeObject<GibeLinkPickerData>(contentValue)?.AsEnumerableOfOne()
+                   ?? Enumerable.Empty<GibeLinkPickerData>();
+        }
+    }
 }
 
